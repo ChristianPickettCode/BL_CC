f3abfe451c00d4b529f4de4031e9a962a1444b9c
Christian <christianpckett@live.ca>
2020-03-03 17:43:26 -0500

    Updates

---
+++ precache-manifest.2e7b06ddfe095c7162872d9ea31b7ba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8392290d8466653ec153c3ad6be2fded",
+    "url": "/ChristianPickettCode/BL_CC/index.html"
+  },
+  {
+    "revision": "b89c45f3b9cb4c5ce33e",
+    "url": "/ChristianPickettCode/BL_CC/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ae2ddcd422f01bf83b4e",
+    "url": "/ChristianPickettCode/BL_CC/static/css/main.14ae1e12.chunk.css"
+  },
+  {
+    "revision": "b89c45f3b9cb4c5ce33e",
+    "url": "/ChristianPickettCode/BL_CC/static/js/2.c414e66c.chunk.js"
+  },
+  {
+    "revision": "4f8a66b46de39b386b72f68888ad10ab",
+    "url": "/ChristianPickettCode/BL_CC/static/js/2.c414e66c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2ddcd422f01bf83b4e",
+    "url": "/ChristianPickettCode/BL_CC/static/js/main.98e98883.chunk.js"
+  },
+  {
+    "revision": "e78ca104fb2ecd96912d",
+    "url": "/ChristianPickettCode/BL_CC/static/js/runtime-main.5fb59532.js"
+  }
+]);
